1befc53d2685270ee2b3b5f9e2228b5eea0ba39c
Markus Peitl <office@markuspeitl.com>
2021-08-23 00:24:42 +0200

    Adapted identifier priority to last

--- src/models/ast-parser/impl/ordo/features/identifier-syntax.ts
+++ src/models/ast-parser/impl/ordo/features/identifier-syntax.ts
@@ -4,7 +4,7 @@ import { BaseAstParser } from '../../../abstract/base-ast-parser';
 import { BaseSyntaxFeature } from '../../../abstract/base-syntax-feature';
 
 export class IdentifierSyntax extends BaseSyntaxFeature {
-	public priority: number = 100;
+	public priority: number = 200;
 	//private regExp: RegExp = new RegExp(/^[a-zA-Z0-9_]+/);
 	public isFeatureDetected(code: string): boolean {
 		const trimmed: string = code.trim();
